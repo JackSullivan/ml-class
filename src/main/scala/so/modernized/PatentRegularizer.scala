3a406a1d151672650247d0c38ced009d5b2d8333
John Sullivan <john.t.sullivan@gmail.com>
2013-11-25 04:09:52 -0500

    Updated PatentRegularizer to make it properly lazy. Should no longer have as many OOM errors.

--- src/main/scala/so/modernized/PatentRegularizer.scala
+++ src/main/scala/so/modernized/PatentRegularizer.scala
@@ -7,33 +7,18 @@ import scala.collection.mutable
  * takes a stream(iterator) of patents and draws until a given number of each label class are drawn. Warns if it gets to the end without the numbers
  */
 class PatentRegularizer(number:Int, labelFunc:(Patent => Patent.Label)) {
+
   def apply(patents:Iterator[Patent]):Iterator[Patent] = {
-    //val patents = _patents.iterator
-    val dimensionSize = labelFunc(patents.next()).domain.dimensionSize
-    val patentBoxes = new mutable.ArrayBuffer[mutable.ArrayBuffer[Patent]](dimensionSize)
-    (0 until dimensionSize).foreach{ idex =>
-      patentBoxes += new mutable.ArrayBuffer[Patent](100)
-    }
-    patents.takeWhile{ patent =>
-      val index = labelFunc(patent).intValue
-      if(patentBoxes(index).size < number) {
-        patentBoxes(index) += patent
-      }
-      !(0 until dimensionSize).foldLeft(true){case (running, idex) => running && patentBoxes(idex).size == number}
-    }
-    /*
-    while (!(0 until dimensionSize).foldLeft(true){case (running, idex) => running && patentBoxes(idex).size == number} && patents.hasNext) {
-      val patent = patents.next()
-      val index = labelFunc(patent).intValue
-      if(patentBoxes(index).size < number) {
-        patentBoxes(index) += patent
+    val counts = new mutable.HashMap[String, Int].withDefaultValue(number)
+    patents.flatMap { patent =>
+      val label = labelFunc(patent).categoryValue
+      counts(label) -= 1
+      if(counts(label) >= 0) {
+        Some(patent)
+      } else {
+        None
       }
     }
-    */
-    if(!(0 until dimensionSize).foldLeft(true){case (running, idex) => running && patentBoxes(idex).size == number}) {
-      println("WARNING! we didn't find enough of every class")
-    }
-    patentBoxes.flatten.iterator
   }
 }
 
