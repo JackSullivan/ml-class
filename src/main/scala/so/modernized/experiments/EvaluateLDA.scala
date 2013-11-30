cd8d363bc64f811c66bf966a4cddb256ff9c2da3
Caitlin Cellier <Caitlin.Cellier@gmail.com>
2013-11-30 17:29:27 -0500

    Topics weren't grouping, trying to fix this error by get the label right away

--- src/main/scala/so/modernized/experiments/EvaluateLDA.scala
+++ src/main/scala/so/modernized/experiments/EvaluateLDA.scala
@@ -9,13 +9,13 @@ import so.modernized.Patent.Label
  * Time: 12:46 PM
  */
 class EvaluateLDA(patents: Iterable[Patent], numTopics: Int) {
-  val patentTopicGroups = patents.groupBy(_.unsupervisedLabel).map{
+  val patentTopicGroups = patents.groupBy(_.unsupervisedLabel.get).map{
     topic => new TopicResult(topic._1,topic._2,topic._2.groupBy(_.iprcLabel).mapValues(_.size.toDouble))
   }
   patentTopicGroups.foreach{
-    topic => println("Topic " + topic.label.get + ":\n")
+    topic => println("Topic " + topic.label + ":\n")
       topic.officialGroupCounts.foreach{ case (offGroup,count) => println("Official Group: " + offGroup + " = " + count)}
   }
-  patentTopicGroups.foreach{topic => println("Accuracy " + topic.label.get + ": " + topic.officialGroupCounts.maxBy(_._2)._2/topic.patents.size)}
+  patentTopicGroups.foreach{topic => println("Accuracy " + topic.label + ": " + topic.officialGroupCounts.maxBy(_._2)._2/topic.patents.size)}
 }
-case class TopicResult(label: Option[Label], patents:Iterable[Patent], officialGroupCounts: Map[Patent.Label,Double])
+case class TopicResult(label: Patent.Label, patents:Iterable[Patent], officialGroupCounts: Map[Patent.Label,Double])
