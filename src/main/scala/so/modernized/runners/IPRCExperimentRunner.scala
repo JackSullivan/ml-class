da6f3abcc28d0977963bb042d330f3de2254d380
John Sullivan <john.t.sullivan@gmail.com>
2013-12-04 11:53:26 -0500

    Updated patent runner

--- src/main/scala/so/modernized/runners/IPRCExperimentRunner.scala
+++ src/main/scala/so/modernized/runners/IPRCExperimentRunner.scala
@@ -11,10 +11,11 @@ object IPRCExperimentRunner {
   def main(args:Array[String]) {
     val inDir = args(0)
 
-    val patents = PatentPipeline(inDir).toList
+    val patents = PatentPipeline(inDir).take(10000).toList
     implicit val random = scala.util.Random
     val (train, test) = patents.shuffle.split(0.7)
-    Seq(new MaxEntExperiment(_.iprcLabel), new SVMExperiment(_.iprcLabel), new SVMExperiment(_.iprcLabel))
-      .map {_.runExperiment(train, test)}
+    val res = new MaxEntExperiment(_.iprcLabel).runExperiment(train, test)
+
+    println(res)
   }
 }
